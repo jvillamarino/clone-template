af331415ef5bd85b659db2161c5bcee251022ba4
Jesús Villamarin <jesus.villamarin@zinobe.com>
2020-12-14 13:49:35 -0500

    Remove: jquery and 3rd party

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -5,13 +5,12 @@ import Contact from "../components/Contact.svelte";
 import Header from "../components/Header.svelte";
 import Portafolio from "../components/Portafolio.svelte";
 import Services from "../components/Services.svelte";
-import About from "./about.svelte";
 
 </script>
 
 <svelte:head>
 	<title>Sapper project template</title>
-	<script src="https://use.fontawesome.com/releases/v5.15.1/js/all.js" crossorigin="anonymous"></script>
+	
 </svelte:head>
 
 

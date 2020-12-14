270fd05d781e60a2d3ad316e8c245c42221204f2
Jesús Villamarin <jesus.villamarin@zinobe.com>
2020-12-14 09:29:03 -0500

    Upload project

---
+++ src/routes/_layout.svelte
@@ -0,0 +1,24 @@
+<script>
+	import Footer from '../components/Footer.svelte';
+import Nav from '../components/Nav.svelte';
+
+	export let segment;
+</script>
+
+<style>
+	main {
+		position: relative;
+		width: 100%;
+		background-color: white;
+		margin: 0 auto;
+		box-sizing: border-box;
+	}
+</style>
+
+<Nav {segment}/>
+
+<main>
+	<slot></slot>
+</main>
+
+<Footer />
---
+++ src/routes/index.svelte
@@ -0,0 +1,25 @@
+<script>
+import AboutInfo from "../components/AboutInfo.svelte";
+import Contact from "../components/Contact.svelte";
+
+import Header from "../components/Header.svelte";
+import Portafolio from "../components/Portafolio.svelte";
+import Services from "../components/Services.svelte";
+import About from "./about.svelte";
+
+</script>
+
+<svelte:head>
+	<title>Sapper project template</title>
+	<script src="https://use.fontawesome.com/releases/v5.15.1/js/all.js" crossorigin="anonymous"></script>
+</svelte:head>
+
+
+<Header />
+<AboutInfo/>
+<Services />
+<Portafolio />
+<Contact />
+
+
+
